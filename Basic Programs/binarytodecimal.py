79588022c61b11cde130680c3107afbcb0868b74
rahulkranjan <kjrahul21@gmail.com>
2020-10-02 00:20:40 +0530

    Revert "changes"
    This reverts commit 629ad8998784a34f1a8ffbf2fd464534e5629a4d.

--- Basic Programs/binarytodecimal.py
+++ Basic Programs/binarytodecimal.py
@@ -1,17 +1,8 @@
-def binary_to_decimal(binary): 
-    decimal = 0 
-    binary = list(str(binary)) #convert binary to a list 
-    binary = binary[::-1]      #reverse the list 
-    power = 0   #declare power variable (for 1st elem == 0) 
-    for number in binary: 
-        if number == '1': 
-            decimal += 2**power     
-        power += 1 #increase power by 1    
-    return decimal 
+b_num = list(input("Input a binary number: "))
+value = 0
 
-print("Enter the binary number:")
-x = int(input())
-print(binary_to_decimal(x))
-
-    return int(str(binary),2) #this is probably the most pythonic way, the string of numbers and the integer makes converts it into decimal
-print(binary_to_decimal(100010100010))
+for i in range(len(b_num)):
+	digit = b_num.pop()
+	if digit == '1':
+		value = value + pow(2, i)
+print("The decimal value of the number is", value)
