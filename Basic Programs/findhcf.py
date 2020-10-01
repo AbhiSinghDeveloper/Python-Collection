35bcf3b6bfde88d9254a5344d871d60e0c3d3088
Priyansh2001here <57267750+Priyansh2001here@users.noreply.github.com>
2020-10-02 00:31:20 +0530

    Update findhcf.py

--- Basic Programs/findhcf.py
+++ Basic Programs/findhcf.py
@@ -1,12 +1,15 @@
-def hcf(x, y):  
-   if x > y:  
-       smaller = y  
-   else:  
-       smaller = x  
-   for i in range(1,smaller + 1):  
-       if((x % i == 0) and (y % i == 0)):  
-           hcf = i  
-   return hcf  
+#Function to return HCF of Two Numbers
+
+# This function takes two integers a & b and returns a integer
+#This function uses recursion for this
+def hcf(a : int,b : int) -> int:
+   # termination condition 
+   # if one number is 0 then other number is HCF of these 2 numbers
+    if a == 0:
+        return b
+   #If first number is not 0 then first number bacomes second%first
+    return hcf(b % a, a)
+ 
   
 num1 = int(input("Enter first number: "))  
 num2 = int(input("Enter second number: "))  
